90d136f8304dbe30c3ffbea14049cc05e5aee4c2
developer <developer@developerM2.local>
2025-04-09 22:21:49 -0400

    feat: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class', // 👈 Modo oscuro por clase
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
