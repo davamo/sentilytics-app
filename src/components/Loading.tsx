1e4fef9640a656294350143354359f56fe003db8
developer <developer@developerM2.local>
2025-04-09 23:05:45 -0400

    feat: componnet Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { CircularProgress } from '@mui/material';
+
+const Loading = () => (
+  <div className="flex justify-center mt-6">
+    <CircularProgress />
+  </div>
+);
+
+export default Loading;
